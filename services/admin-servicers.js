5daabde7dd527347144fe9e0fb50daf2efdaf4b3
lavender <id0421942@gmail.com>
2022-02-10 15:15:31 +0800

    add create restaurant api

--- services/admin-servicers.js
+++ services/admin-servicers.js
@@ -1,5 +1,7 @@
 const { Restaurant, Category } = require('../models')
 
+const { imgurFileHandler } = require('../helpers/file-helpers')
+
 const adminController = {
   getRestaurants: (req, cb) => {
     Restaurant.findAll({
@@ -8,6 +10,24 @@ const adminController = {
       include: [Category]
     })
       .then(restaurants => cb(null, { restaurants }))
+  },
+
+  postRestaurant: (req, cb) => {
+    const { name, tel, address, openingHours, description, categoryId } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+    const { file } = req
+    imgurFileHandler(file)
+      .then(filePath => Restaurant.create({
+        name,
+        tel,
+        address,
+        openingHours,
+        description,
+        image: filePath || null,
+        categoryId
+      }))
+      .then(newRestaurant => cb(null, { restaurant: newRestaurant }))
+      .catch(err => cb(err))
   },
 
   deleteRestaurant: (req, cb) => {
